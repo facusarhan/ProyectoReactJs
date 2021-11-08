29189af3a72d47dcbd9e5c8c99b468beec08e4fa
facundo sarhan <facu.sarhan2000@gmail.com>
2021-11-08 19:48:10 -0300

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import './NavBar.css';
+
+export const NavBar = () => {
+    
+    return (
+        <header className="mi-app">
+        <h1>Fudo</h1>
+        <nav>
+            <ul className="nav">
+                <li><a href="">Home</a></li>
+                <li><a href="">Tienda</a></li>
+                <li><a href="">Informacion</a></li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
