2e5c7d91909dff7d467439a82947bbd04c422f9f
facundo sarhan <facu.sarhan2000@gmail.com>
2021-11-22 14:54:58 -0300

    boostrap funciona

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavBar} from './components/navBar/NavBar';
 import {ItemListContainer} from './components/ItemListContainer/itemListContainer';
 
